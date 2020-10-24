dc9e47f305694c1359d9d3818b595a7a2ff6b425
ibishop <isaiahjonbishop@gmail.com>
2020-10-24 03:11:57 -0300

    Added Working Table Added Cosmetic Radio Bar Added Cosmetic Geography Search

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ import pandas as pd
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-from utils.mapbox import figure_factory,figure_data
-from utils.dash import year_slider, buttons
+from utils.mapbox import figure_factory
+from utils.dash import year_slider, buttons, build_table,table_update, geography_searchbar,sort_menu
 from utils.cleaning import filter_year
 from dash.dependencies import Input, Output
+from dash.exceptions import PreventUpdate
 
 # Load CSS
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -27,11 +28,14 @@ app.layout = html.Div([
             html.Div(buttons( [ x for x in data.columns[3:] if "graph" in x]),id='buttons'),
             html.Div(dcc.Graph(id='map', figure=fig,config={'scrollZoom': True})),
             html.Div(year_slider(all_data.year))
-        ], style={'height': 700, 'width': 700}),
+        ], style={'height': 700, 'width': 650}),
 
         html.Div([
-            html.Div(id='selected-data')
-        ], style={'height': 700, 'width': 700})
+            html.Div(geography_searchbar(data['Geography']), id='geo-bar'),
+            html.Div(sort_menu(), id='sort-radial', style={'paddingTop': '0.1cm'}),
+            html.Div(build_table(),id='selected-data', style={'height': 400 , 'width': 400})
+        ], style={'height': 700, 'width': 700, 'rowCount':3})
+
     ],style={'columnCount': 2}),
 ])
 
@@ -48,11 +52,17 @@ def update_figure(selected_year,column):
     return fig
 
 @app.callback(
-    Output('selected-data', 'children'),
-    [Input('map', 'selectedData')])
-def select_point(points):
-    return str(points)
+    [Output('table', 'columns'),
+     Output('table', 'data')],
+    [Input('map', 'selectedData'),
+     Input('selected-column','value')])
+def display_selection(selection,column):
 
+    if not selection:
+        raise PreventUpdate
 
+    indicies =  [ x['pointIndex'] for x in selection['points'] ]
+    temp = data.loc[indicies]
+    return table_update(temp,column)
 
 app.run_server(debug=True, use_reloader=False)
